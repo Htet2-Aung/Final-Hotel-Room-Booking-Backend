77628bccd0e1c191c87d5a09cd3da7161137d4b3
HtetHtetAung <htethtet23999@gmail.com>
2023-05-24 15:24:10 +0630

    first commit

---
+++ src/main/java/com/hostmdy/hotelbooking/HotelRoomBookingSeverApplication.java
@@ -0,0 +1,13 @@
+package com.hostmdy.hotelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomBookingSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelRoomBookingSeverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hostmdy/hotelbooking/HotelRoomBookingSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostmdy.hotelbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRoomBookingSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
